2367a6d6b03d7a5f625756e0b3842f0381e8e5c9
Chris Kuo <chris.kuo@chriskuo.com>
2018-07-21 19:14:49 -0700

    implemented search() and added new test cases to test()

--- reg_exp.py
+++ reg_exp.py
@@ -1,7 +1,12 @@
 
 def search(pattern, text):
     "Return True if pattern appears anywhere in text"
-    return match('.*' + pattern, text)
+    if pattern.startswith('^'):
+        # pattern must occur at start of text
+        return match(pattern[1:], text)
+    else:
+        # pattern can occur anywhere in the text
+        return match('.*' + pattern, text)
 
 
 def match(pattern, text):
@@ -19,3 +24,8 @@ def test():
     assert search('^start', 'not the start') == False
     assert match('start', 'not the start') == False
     assert match('a*b*c*', 'just anything')
+    assert match('x?', 'text')
+    assert match('text?', 'text')
+    assert match('text?', 'tex')
+
+    def words(text): return text.split()
